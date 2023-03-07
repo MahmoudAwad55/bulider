af346410e9340dc587fe12a6dd88d5a923d56b65
Mahmoud <mahmoudawoud2010@gmail.com>
2023-03-07 17:10:19 +0200

    first commit

---
+++ src/UserBuilder.java
@@ -0,0 +1,29 @@
+public class UserBuilder {
+    public static void main(String[] args) 
+{
+	User user1 = new User.UserBuilder("ahmed", ",mostafa")
+	.age(30)
+	.phone("1234567")
+	.address("Fake address 1234")
+	.build();
+
+	System.out.println(user1);
+
+	User user2 = new User.UserBuilder("mahmoud", "omar")
+	.age(40)
+	.phone("5655")
+	//no address
+	.build();
+
+	System.out.println(user2);
+
+	User user3 = new User.UserBuilder("Khalifa", "mohamed")
+	//No age
+	//No phone
+	//no address
+	.build();
+
+	System.out.println(user3);
+}
+    
+}
